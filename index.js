d390480ef9abc941bc16fbba4be8562ed6510355
trafdual <traz08102003@gmail.com>
2024-12-04 01:31:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const {
+  allowInsecurePrototypeAccess
+} = require('@handlebars/allow-prototype-access')
+const Handelbars = require('handlebars')
+const hbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const path = require('path')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo')
+const userRouter= require('./routes/UserRoutes')
+
+var app = express()
+
+app.engine(
+  '.hbs',
+  hbs.engine({
+    extname: 'hbs',
+    defaultLayout: false,
+    layoutsDir: 'views/layouts/',
+    handlebars: allowInsecurePrototypeAccess(Handelbars)
+  })
+)
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(methodOverride('_method'))
+
+const uri =
+  'mongodb+srv://webbongda:webbongda2024@webbongda.me9wj.mongodb.net/weboto?retryWrites=true&w=majority&appName=webbongda'
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(console.log('kết nối thành công'))
+
+const mongoStoreOptions = {
+  mongooseConnection: mongoose.connection,
+  mongoUrl: uri,
+  collection: 'sessions'
+}
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+  session({
+    secret: 'mysecretkey',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create(mongoStoreOptions),
+    cookie: {
+      secure: false
+    }
+  })
+)
+app.use(cors())
+app.use(express.static(path.join(__dirname, '/uploads')))
+
+app.use('/',userRouter)
+
+app.listen(8080, () => {
+  try {
+    console.log('kết nối thành công 8080')
+  } catch (error) {
+    console.log('kết nối thất bại 8080', error)
+  }
+})
---
+++ routes/UserRoutes.js
@@ -0,0 +1,83 @@
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const User = require('../models/UserModel')
+router.post('/register', async (req, res) => {
+  try {
+    const { hovaten, email, password, role, phone } = req.body
+
+    // Kiểm tra số điện thoại
+    if (!phone || !/^\d{10}$/.test(phone)) {
+      return res.status(400).json({ message: 'Số điện thoại không hợp lệ' })
+    }
+    const exitphone = await User.findOne({ phone })
+    if (exitphone) {
+      return res.status(400).json({ message: 'Số điện thoại đã được đăng kí' })
+    }
+
+    // Kiểm tra email đúng định dạng
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (!email || !emailRegex.test(email)) {
+      return res.status(400).json({ message: 'Email không hợp lệ' })
+    }
+
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email đã được đăng ký' })
+    }
+
+    // Kiểm tra mật khẩu mạnh
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/
+    if (!password || !passwordRegex.test(password)) {
+      return res.status(400).json({
+       message:
+          'Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt'
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = new User({
+      hovaten,
+      email,
+      password: hashedPassword,
+      role,
+      phone
+    })
+    await user.save()
+    res.json(user)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Đã xảy ra lỗi.' })
+  }
+})
+
+router.post('/loginfull', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      res.json({ message: 'email không chính xác' })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      res.json({ message: 'nhập sai mật khẩu' })
+    }
+
+    if (user.role === 'admin') {
+      res.json({ role: 'admin', user: user })
+    } else if (user.role === 'staff') {
+      res.json({ role: 'staff', user: user })
+    } else {
+      res.json({ role: 'user', user: user })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Đã xảy ra lỗi.' })
+  }
+})
+
+module.exports = router
